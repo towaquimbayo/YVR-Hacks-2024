14ff40624adc0cac1ff9aff74f83995dcf244725
noufilsaqib <noufil.s@hotmail.com>
2024-04-06 13:40:44 -0700

    Setup login route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import LiveMonitor from "./pages/LiveMonitor";
 import Issue from "./pages/Issue";
 import Issues from "./pages/Issues";
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="login" element={<Login />} />
       <Route path="live-monitor" element={<LiveMonitor />} />
       <Route path="issue" element={<Issue />} />
       <Route path="issues" element={<Issues />} />
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+        <div className="text-3xl text-center mt-10">
+            <h1>Login</h1>
+        </div>
+    );
+}
