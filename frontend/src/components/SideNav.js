9fb47e7d99ef1020f05ce84add7982ce92bd78ca
Towa Quimbayo <towacurtis@hotmail.com>
2024-04-10 14:18:35 -0700

    fixed navlinks

--- frontend/src/components/SideNav.js
+++ frontend/src/components/SideNav.js
@@ -11,17 +11,17 @@ export default function SideNav() {
       <div>
         <h1 className="text-2xl font-bold pl-3">Crow's Nest</h1>
         <ul className="flex flex-col gap-2 pt-16">
-          <li className="rounded-lg py-4 px-2">
+          <li className="rounded-lg py-4 px-2 hover:bg-primary hover:bg-opacity-10 hover:text-primary">
             <NavLink to="/" className="text-md flex items-center gap-3">
               <LayoutGrid size={18} /> Dashboard
             </NavLink>
           </li>
-          <li className="rounded-lg py-4 px-2">
+          <li className="rounded-lg py-4 px-2 hover:bg-primary hover:bg-opacity-10 hover:text-primary">
             <NavLink to="/reports" className="text-md flex items-center gap-3">
               <ClipboardList size={18} /> Reports
             </NavLink>
           </li>
-          <li className="rounded-lg py-4 px-2">
+          <li className="rounded-lg py-4 px-2 hover:bg-primary hover:bg-opacity-10 hover:text-primary">
             <NavLink
               to="/live-monitor"
               className="text-md flex items-center gap-3"
@@ -29,17 +29,16 @@ export default function SideNav() {
               <Monitor size={18} /> Live Monitor
             </NavLink>
           </li>
+          <li className="rounded-lg py-4 px-2 hover:bg-primary hover:bg-opacity-10 hover:text-primary">
+            <Link
+              onClick={async () => dispatch(clearSession())}
+              to="/login"
+              className="text-md flex items-center gap-3"
+            >
+              <LogOut size={18} /> Logout
+            </Link>
+          </li>
         </ul>
-      </div>
-
-      <div className="pl-3">
-        <Link
-          onClick={async () => dispatch(clearSession())}
-          to="/login"
-          className="text-md flex items-center gap-3"
-        >
-          <LogOut size={18} /> Logout
-        </Link>
       </div>
     </div>
   );
