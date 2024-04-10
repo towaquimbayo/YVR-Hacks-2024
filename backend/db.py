fc8f56e44fea504f10b62f76e24afada4f941270
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-04-09 21:59:58 -0700

    no need

--- backend/db.py
+++ backend/db.py
@@ -1,25 +1,10 @@
-import sqlite3
+# import sqlite3
+#
+#
+# class Db:
+#     db = None
+#
+#     @classmethod
+#     def add_app(cls, app):
+#         cls.db = SQLAlchemy(app)
 
-
-class Db:
-    def __init__(self):
-        self.conn = None
-        self.cursor = None
-        self.int()
-
-    def int(self):
-        self.conn = sqlite3.connect('yvr_hack.db')
-        self.cursor = self.conn.cursor()
-
-        # Create table if it does not exist
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS counts (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            timestamp TEXT DEFAULT CURRENT_TIMESTAMP,
-                            in_count INTEGER NOT NULL,
-                            out_count INTEGER NOT NULL
-                        )''')
-        self.conn.commit()
-
-    def do_query(self, in_count, out_count):
-        self.cursor.execute("INSERT INTO counts (in_count, out_count) VALUES (?, ?)", (in_count, out_count))
-        self.conn.commit()
