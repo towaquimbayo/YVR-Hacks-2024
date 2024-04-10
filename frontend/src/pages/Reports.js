7e97c2b89a95cdff8b22c6743d3caddf2a3eefa8
Towa Quimbayo <towacurtis@hotmail.com>
2024-04-10 14:13:10 -0700

    setup for live video fetching

--- frontend/src/pages/Reports.js
+++ frontend/src/pages/Reports.js
@@ -34,7 +34,6 @@ export default function Reports() {
     });
   }, [reports, selectedTab]);
 
-  // Mock fetch reports
   useEffect(() => {
     async function fetchReports() {
       const response = await fetch(
