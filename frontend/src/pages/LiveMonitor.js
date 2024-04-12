179fd395b2a5191b1354daa8f6d9cc38458ccc59
Towa Quimbayo <towacurtis@hotmail.com>
2024-04-11 19:13:29 -0700

    Connected live video page

--- frontend/src/pages/LiveMonitor.js
+++ frontend/src/pages/LiveMonitor.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import ReactPlayer from "react-player";
 import Layout from "../components/Layout";
 
 export default function LiveMonitor() {
@@ -12,25 +13,29 @@ export default function LiveMonitor() {
     if (!isLoggedIn) navigate("/login");
   }, [isLoggedIn, navigate]);
 
-  useEffect(() => {
-    async function fetchReports() {
-      const response = await fetch(
-        process.env.REACT_APP_SERVER_ENDPOINT + "/video"
-      );
-      // const data = await response.json();
-      console.log("Fetched Live Video Response:", response);
-    }
-    fetchReports();
-  }, []);
-
   return (
     <Layout
       title="Live Monitor"
       heading={<h1 className="text-3xl font-semibold">Live Monitor</h1>}
     >
       <p className="text-lg">
-        Welcome to Crow's Nest! This is a live monitor page.
+        View the live video feed from the security camera.
       </p>
+      <div className="mt-4 max-w-[800px]">
+        <ReactPlayer
+          url={{
+            src: process.env.REACT_APP_SERVER_ENDPOINT + "/video",
+            type: "multipart/x-mixed-replace",
+          }}
+          muted
+          playing
+          controls
+          width="100%"
+          height="100%"
+          onError={(e) => console.log("Error:", e)}
+          fallback={<p>Your browser does not support the video tag.</p>}
+        />
+      </div>
     </Layout>
   );
 }
